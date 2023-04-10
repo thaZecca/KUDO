fc367f97b0d697a20eac3b6b0dce431149687c5f
Simone Zecchinato <105296270+thaZecca@users.noreply.github.com>
2023-04-10 07:10:21 +0000

    backend ridistributore pt1 finito

--- ridistributore/ridistributoreVerifica.php
+++ ridistributore/ridistributoreVerifica.php
@@ -10,6 +10,36 @@
     $conn = new mysqli($host, $username, $password, $db_nome) or die($conn -> error);
     $conn -> select_db($db_nome) or die($conn -> error);
 
-    
+    $idDaCorreggere=array();
+    $idDaVerificare=array();
+
+    $ris = $conn -> query('SELECT ID_NC FROM non_conformita WHERE UserCorrezione IS NULL');
+    $num = $ris -> num_rows;
+
+    for($i=0; $i<$num; $i++){
+        $row = $ris -> fetch_assoc();
+        $idDaCorreggere[]=$row['ID_NC'];
+    }
+
+    $ris = $conn -> query('SELECT ID_NC FROM non_conformita WHERE UserVerifica IS NULL AND isCorretta=1');
+    $num = $ris -> num_rows;
+
+    for($i=0; $i<$num; $i++){
+        $row = $ris -> fetch_assoc();
+        $idDaVerificare[]=$row['ID_NC'];
+    }
+
+    foreach($idDaCorreggere as $k => $v){
+        if($_POST['scadCorr'.$v]!='' AND $_POST['asseCor'.$v]!='') 
+            $conn -> query("UPDATE non_conformita SET UserCorrezione='".$_POST['asseCor'.$v]."', Azione_Correttiva='".$_POST['azcorr'.$v]."', DataScadenza='".$_POST['scadCorr'.$v]."' WHERE ID_NC=".$v);
+    }
+    echo '<br>';
+
+    foreach($idDaVerificare as $k => $v){
+        if($_POST['scadVer'.$v]!='' AND $_POST['asseVer'.$v]!='') 
+            $conn -> query("UPDATE non_conformita SET UserVerifica='".$_POST['asseVer'.$v]."', DataScadenza='".$_POST['scadVer'.$v]."' WHERE ID_NC=".$v);
+    }
+
+    header('location: index.php');
 
 ?>
