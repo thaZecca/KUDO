1f7a99657cbdf01e173febc6ffc2979426799c8d
Simone Zecchinato <105296270+thaZecca@users.noreply.github.com>
2023-04-26 11:53:54 +0200

    Update otp.php

--- login/OTP/otp.php
+++ login/OTP/otp.php
@@ -40,8 +40,8 @@
       $mail->isSMTP();                                            //Send using SMTP
       $mail->Host       = 'authsmtp.securemail.pro';                     //Set the SMTP server to send through
       $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-      $mail->Username   = 'service@kudokudo.it';                     //SMTP username
-      $mail->Password   = 'Kudokudo2023';                               //SMTP password
+      $mail->Username   = '******';                     //SMTP username
+      $mail->Password   = '*****';                               //SMTP password
       $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
       $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
   
