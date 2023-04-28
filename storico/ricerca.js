21f1110cee22e6fb6c63bae30e6921310a46068f
vivacetto <123804503+vivacetto@users.noreply.github.com>
2023-04-28 12:21:42 +0200

    Add files via upload

---
+++ storico/ricerca.js
@@ -0,0 +1,17 @@
+function ricerca() {
+    var temp = document.getElementById("ricerca").innerHTML;
+    document.getElementById("ricerca").innerHTML = "";
+
+    var file = JSON.parse(temp);
+    var testo = '<div class="container-fluid table-responsive"><h1 class="display-5 fw-bold">Storico non conformità</h1><br><table class="table table-striped table-hover"><tr><th>ID</th><th>User Riscontro</th><th>Origine</th><th>Causa</th><th>User Correzione</th><th>Azione Correttiva</th><th>Data Scadenza</th><th>isCorretta</th><th>User Verifica</th><th>isVerificata</th><th>isConclusa</th></tr>';
+
+    for (var i = 0; i < file.length; i++) {
+      var nc = file[i];
+      testo += '<tr><td>'+nc['ID_NC']+'</td><td>'+nc['UserRiscontro']+'</td>';
+      if (nc['isInterna']) testo += '<td>'+nc['Nome_Reparto']+'</td>';
+      else testo += '<td>'+nc['ID_Fornitore']+'</td>';
+      testo += '<td>'+nc['Causa']+'</td><td>'+nc['UserCorrezione']+'</td><td>'+nc['Azione_Correttiva']+'</td><td>'+nc['DataScadenza']+'</td><td>'+nc['isCorretta']+'</td><td>'+nc['UserVerifica']+'</td><td>'+nc['isVerificata']+'</td><td>'+nc['isConclusa']+'</td></tr>';
+    }
+    testo += '</table></div>';
+    document.getElementById("testo").innerHTML = testo;
+}
