aef15ebfdfbdd543556c8698990bd3a601ef3792
vivacetto <123804503+vivacetto@users.noreply.github.com>
2023-04-29 10:08:45 +0200

    Update chisiamo.php

--- chisiamo.php
+++ chisiamo.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Chi siamo</title>
     <link rel="icon" href="./resources/logo.png">
-  </head>
 
 <link href="./resources/css/bootstrap.min.css" rel="stylesheet">
 
