0e86415dd307e48762796efab57d5e46cd377509
vivacetto <123804503+vivacetto@users.noreply.github.com>
2023-04-29 10:09:45 +0200

    Update index.php

--- ridistributore/index.php
+++ ridistributore/index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard - Ridistributore</title>
     <link rel="icon" href="../resources/logo.png">
-  </head>
  
   <?php 
         session_start();
